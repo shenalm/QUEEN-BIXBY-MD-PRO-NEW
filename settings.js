e9545c7ca87bea1799798c77a4ac1cd1e85f1fc2
shenalm <118654149+shenalm@users.noreply.github.com>
2022-11-20 12:46:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
+global.owner = ['94772869894']
+global.premium = ['94772869894']
+global.ownernomer = '94772869894'
+global.ownername = 'Dark S.CHANDI'
 global.botname = 'BIXBY-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
